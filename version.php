5b451f5ba5dae94a98657edabf316af228b3f652
TSUCHITANI Kazuko <tsuchi000@gmail.com>
2012-04-28 20:51:05 +0900

    Version 1.1.0

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-$_ASSIST_CONF['version'] = '1.1.0.test20120409';          // Plugin Version
+$_ASSIST_CONF['version'] = '1.1.0';          // Plugin Version
 //Previous　$_ASSIST_CONF['version'] = '1.1.0.test20120215';   
 ?>
